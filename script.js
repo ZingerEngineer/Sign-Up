099c0f0b7225c84197e14cc25c6336e6729a3af3
ZingerEngineer <ramyisez@gmail.com>
2021-12-02 19:28:18 +0200

    okay lol

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let firstName = document.querySelector("#first-name")
 let lastName = document.querySelector("#last-name")
-
+let email = document.querySelector(".input-email")
+let emailReg = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
 let password = document.querySelector(".input-password");
 let isVisible = false;
 let visButton = document.querySelector(".visibility")
-let firstNameReg = new RegExp(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u);
-let validName = true;
+const firstNameReg = new RegExp(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u);
+const lastNameReg = new RegExp(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u);
+const passwordReg = new RegExp(/^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&?@ "]).*$/)
+const specialReg = new RegExp(/^.*(?=.*[!#$%&?@ "]).*$/)
+let validFirstName = null;
+let validLastName = null;
+let validEmail = null;
+let validPassowrd = null;
+let eightCharacters = document.querySelector("#spec1")
+let oneSpecial = document.querySelector("#spec2")
+let upperCase = document.querySelector("#spec3")
+let lowerCase = document.querySelector("#spec4")
+let numbers = document.querySelector("#spec5")
 let buttonIcon = document.getElementsByTagName("i")[0];
 
 let visibility = ()=>{
@@ -25,13 +37,67 @@ if (isVisible === false){
 }
 visButton.addEventListener("click", visibility)
 
-let nameValidation = ()=>{
+let passwordSpec1= ()=>{
+if(password.value.length == 8)
+{
+    eightCharacters.removeAttribute("class")
+    eightCharacters.setAttribute("class", "valid")
+}else if (password.value.length < 8)
+{
+    eightCharacters.removeAttribute("class")
+    eightCharacters.setAttribute("class", "inValid") 
+}
+
+}
+
+let passwordSpec2 = ()=>{
+if(specialReg.test(password.value) == false)
+{
+    oneSpecial.removeAttribute("class")
+    oneSpecial.setAttribute("class", "inValid")
+}else if (specialReg.test(password.value) == true){
+    eightCharacters.removeAttribute("class")
+    eightCharacters.setAttribute("class", "valid") 
+}
+
+    
+}
+
+
+password.addEventListener("input", passwordSpec1)
+password.addEventListener("input", passwordSpec2)
+
+let formValidation = ()=>{
+    if(emailReg.test(email.value) === true)
+    {
+        validEmail = true;
+    }
+    else if(emailReg.test(email.value) === false)
+    {
+        validEmail = false;
+    }
 if(firstNameReg.test(firstName.value) === true)
 {
-validName = true;
+validFirstName = true;
 }
 else if (firstNameReg.test(firstName.value) === false)
 {
-validName = false;
+validFirstName = false;
+}
+if(lastNameReg.test(lastName.value) === true)
+{
+validLastName = true;
+}
+else if (lastNameReg.test(lastName.value) === false)
+{
+validLastName = false;
+}
+if(passwordReg.test(password.value) === true)
+{
+validLastPassword = true;
+}
+else if (passwordReg.test(password.value) === false)
+{
+validPassowrd = false;
 }
 }
